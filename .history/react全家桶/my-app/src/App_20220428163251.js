6554f7d7e2220fe13a15a41876a41323b0dc715f
wanghuitingg <1687752378@qq.com>
2022-04-28 17:04:58 +0800

    react

--- .history/react全家桶/my-app/src/App_20220428163251.js
+++ .history/react全家桶/my-app/src/App_20220428163251.js
@@ -29,19 +29,19 @@ function App() {
       {/**路由匹配默认是： 模糊匹配
          * exact 精确匹配  只有路径完全相同才会被匹配到
          */}
-      <Route path='/' exact component={Home} />
+      {/* <Route path='/' exact component={Home} />
       <Route path='/about' component={About} />
       <Route path='/test' component={Test} />
-      <Route path='/active/:id' component={Active} />
+      <Route path='/active/:id' component={Active} /> */}
 
 
       {/* *从上到下匹配，找到就停止，仅渲染与当前位置匹配的第一个元素
        * 所以，path='/'必须放在最后
        */}
-      {/* <Switch>
-        <Route path='/about' component={About} />
+      <Switch>
         <Route path='/' component={Home} />
-      </Switch> */}
+        <Route path='/about' component={About} />
+      </Switch>
 
 
     </div>
